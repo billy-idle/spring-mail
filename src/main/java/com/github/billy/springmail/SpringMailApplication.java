f24344aa677361517f09126494f7214f468b70f8
gguzmans <gguzmans@emeal.nttdata.com>
2022-11-23 17:53:49 -0500

    Se agrega el servicio para envio de correos

--- src/main/java/com/github/billy/springmail/SpringMailApplication.java
+++ src/main/java/com/github/billy/springmail/SpringMailApplication.java
@@ -1,7 +1,10 @@
 package com.github.billy.springmail;
 
+import com.github.billy.springmail.service.EmailService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringMailApplication {
@@ -10,4 +13,11 @@ public class SpringMailApplication {
     SpringApplication.run(SpringMailApplication.class, args);
   }
 
+  @Bean
+  CommandLineRunner runner(EmailService emailService) {
+    return args -> {
+      emailService.sendSimpleMessage("guillesupremacy@gmail.com", "First Email", "This is gonna "
+          + "be great!");
+    };
+  }
 }
